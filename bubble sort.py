a1e8c03119a03758a0c55f1b1e79df5f6bc4465d
michWangOfficial <144123595+michWangOfficial@users.noreply.github.com>
2023-12-12 02:12:00 +0000

    Update bubble sort.py
    now can calculate time used on it

--- bubble sort.py
+++ bubble sort.py
@@ -1,3 +1,10 @@
+import math
+import random
+import time
+
+y = 0
+x = 0
+
 def convert(string):
     li = list(string.split(" "))
     return li
@@ -11,15 +18,18 @@ def bubbleSort(thelist):
                 thelist[position] = thelist[position+1]
                 thelist[position+1] = temp
 
-print ("Enter the list with space between items")
-list1 = input()
-list1 = convert(list1)
-print (list1)
+list1 = []
+for index in range(0,10000):
+    list1.append(random.randint(0,999999))
 
 for index in range(0, len(list1)):
     list1[index] = int(list1[index])
 
-print (list1)
+x = time.time()
 
 bubbleSort(list1)
 print(list1)
+
+y = time.time()
+
+print(y-x)
